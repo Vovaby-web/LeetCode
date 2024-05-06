1bf1f37f162230f4dfc7ae8655c33e1de9451e78
VovaBy-web <vova_by@mail.ru>
2024-05-06 17:53:11 +0300

    my commit 39

--- LeetCode/src/Test.java
+++ LeetCode/src/Test.java
@@ -2,21 +2,45 @@
 import java.util.*;
 public class Test {
   public static void main(String[] args) {
-    System.out.println(characterReplacement("  "));
+    System.out.println(Arrays.toString(medianSlidingWindow(new int[]{1,2,3,4,5,6,7}, 3)));
   }
-  public static int characterReplacement(String s) {
-    int n = s.length();
-    int[] a = new int[33];
-    int j = 0;
-    int max = (n == 0) ? 0 : 1;
+  public static double[] medianSlidingWindow(int[] nums, int k) {
+    int n = nums.length;
+    double[] result = new double[n - k + 1];
+    PriorityQueue<Integer> left = new PriorityQueue<>(Collections.reverseOrder());
+    PriorityQueue<Integer> right = new PriorityQueue<>();
     for (int i = 0; i < n; i++) {
-      int ci = (s.charAt(i)==32)?s.charAt(i):s.charAt(i)-'a';
-      a[ci]++;
-      if (a[ci] > 1) {
-        j = i;
+      if (i >= k) {
+        remove(left, right, nums[i - k]);
       }
-      max = Math.max(max, i - j + 1);
+      add(left, right, nums[i]);
+      if (i >= k - 1) {
+        result[i - (k - 1)] = median(left, right);
+      }
     }
-    return max;
+    return result;
+  }
+  public static void add(PriorityQueue<Integer> left, PriorityQueue<Integer> right, int x) {
+    left.add(x);
+    right.add(left.poll());
+    if (left.size() < right.size()) {
+      left.add(right.poll());
+    }
+  }
+  public static void remove(PriorityQueue<Integer> left, PriorityQueue<Integer> right, int x) {
+    if (x >= right.peek()) {
+      right.remove(x);
+      if (left.size() == right.size() + 2)
+        right.add(left.poll());
+    } else {
+      left.remove(x);
+      if (left.size() < right.size())
+        left.add(right.poll());
+    }
+  }
+  public static double median(PriorityQueue<Integer> left, PriorityQueue<Integer> right) {
+    if (left.size() == right.size())
+      return (double) (left.peek() + right.peek()) / 2.0;
+    else return left.peek();
   }
 }
--- LeetCode/src/Yandex/L9_SlidingWindow/T480/T480_SlidingWindowMedian.java
+++ LeetCode/src/Yandex/L9_SlidingWindow/T480/T480_SlidingWindowMedian.java
@@ -3,7 +3,7 @@ package Yandex.L9_SlidingWindow.T480;
 import java.util.*;
 public class T480_SlidingWindowMedian {
   public static void main(String[] args) {
-    System.out.println(Arrays.toString(medianSlidingWindow2(new int[]{1,2,3,4}, 3)));
+    System.out.println(Arrays.toString(medianSlidingWindow3(new int[]{3, 4, 5, 2, 5, 7}, 5)));
   }
   // Сложность O(n log n)
   public static double[] medianSlidingWindow(int[] nums, int k) {
@@ -25,7 +25,7 @@ public class T480_SlidingWindowMedian {
       else list.add(Math.abs(r + 1), nums[i]); // Добавьте его в ожидаемое положение
       // Вставьте в список result медиану согласно значению k
       if (k % 2 == 0) {
-        result[i - k + 1] = ((double)list.get((k / 2) - 1) + list.get((k / 2))) / 2.0;
+        result[i - k + 1] = ((double) list.get((k / 2) - 1) + list.get((k / 2))) / 2.0;
       } else {
         result[i - k + 1] = list.get((k / 2));
       }
@@ -99,130 +99,143 @@ public class T480_SlidingWindowMedian {
   }
   /**
    * Using TreeSet. (Here time complexity is most optimized)
-   *
+   * <p>
    * Very similar to https://leetcode.com/problems/find-median-from-data-stream/
-   *
+   * <p>
    * Time Complexity: O((N-K)*log K + N*log K) = O(N * log K)
    * Add Elements = O(N*Log K)
    * Remove Elements = O((N-K)*log K) ==> TreeSet.remove() in JAVA is O(log K)
-   *
+   * <p>
    * Space Complexity: O(K)
-   *
+   * <p>
    * N = Length of nums array. K = Input k.
    */
-    public static double[] medianSlidingWindow2(int[] nums, int k) {
-      int n = nums.length;
-      double[] result = new double[n - k + 1];
-      if (k == 1) return Arrays.stream(nums).asDoubleStream().toArray();
-      Comparator<Integer> comparator =
-        (a, b) -> (nums[a] != nums[b] ? Integer.compare(nums[a], nums[b]) : Integer.compare(a, b));
-      TreeSet<Integer> smallNums = new TreeSet<>(comparator.reversed());
-      TreeSet<Integer> largeNums = new TreeSet<>(comparator);
-      for (int i = 0; i < n; i++) {
-        if (i >= k) {
-          removeElement(smallNums, largeNums, nums, i - k);
-        }
-        addElement(smallNums, largeNums, i);
-        if (i >= k - 1) {
-          result[i - (k - 1)] = getMedian(smallNums, largeNums, nums);
-        }
+  // Используются два TreeSet для хранения индексов элементов в массиве nums.
+  // Это позволяет эффективно находить медиану в скользящем окне размером k.
+  // Использование двух TreeSet позволяет быстро находить медиану в скользящем окне
+  // без необходимости сортировки всего окна на каждом шаге.
+  // Вместо этого медиана вычисляется на основе двух TreeSet, которые всегда содержат текущие
+  // максимальный и минимальный элементы в окне.
+  public static double[] medianSlidingWindow2(int[] nums, int k) {
+    int n = nums.length;
+    double[] result = new double[n - k + 1];
+    if (k == 1)
+      return Arrays.stream(nums).asDoubleStream().toArray();
+    Comparator<Integer> comparator =
+      (a, b) -> (nums[a] != nums[b] ? Integer.compare(nums[a], nums[b]) : Integer.compare(a, b));
+    // Один TreeSet (leftNums) хранит индексы элементов, которые находятся слева
+    // от текущего элемента и меньше его.
+    TreeSet<Integer> leftNums = new TreeSet<>(comparator.reversed());
+    // Другой TreeSet (rightNums) хранит индексы элементов, которые находятся справа
+    // от текущего элемента и больше его.
+    TreeSet<Integer> rightNums = new TreeSet<>(comparator);
+    for (int i = 0; i < n; i++) {
+      if (i >= k) {
+        removeElement(leftNums, rightNums, nums, i - k);
       }
-      return result;
-    }
-    private static void addElement(TreeSet<Integer> smallNums, TreeSet<Integer> largeNums, int i) {
-      smallNums.add(i);
-      largeNums.add(smallNums.pollFirst());
-      if (smallNums.size() < largeNums.size()) {
-        smallNums.add(largeNums.pollFirst());
-      }
-    }
-    private static void removeElement(TreeSet<Integer> smallNums, TreeSet<Integer> largeNums,
-                                      int[] nums, int i) {
-      if (largeNums.contains(i)) {
-        largeNums.remove(i);
-        if (smallNums.size() == largeNums.size() + 2) {
-          largeNums.add(smallNums.pollFirst());
-        }
-      } else {
-        smallNums.remove(i);
-        if (smallNums.size() < largeNums.size()) {
-          smallNums.add(largeNums.pollFirst());
-        }
+      addIndex(leftNums, rightNums, i, nums);
+      if (i >= k - 1) {
+        result[i - k + 1] = getMedian(leftNums, rightNums, nums);
       }
     }
-    private static double getMedian(TreeSet<Integer> smallNums, TreeSet<Integer> largeNums, int[] nums) {
-      if (smallNums.size() == largeNums.size()) {
-        return ((double) nums[smallNums.first()] + nums[largeNums.first()]) / 2;
-      }
-      return nums[smallNums.first()];
+    return result;
+  }
+  // При добавлении нового элемента в окно, он сначала добавляется в smallNums, затем перекидывается
+  // в largeNums, если это необходимо для соблюдения баланса размеров двух TreeSet.
+  // [5,2,2,7,3,7,9,0,2,3]
+  // 9
+  private static void addIndex(TreeSet<Integer> leftNums, TreeSet<Integer> rightNums, int i, int[] nums) {
+    leftNums.add(i);
+    rightNums.add(leftNums.pollFirst());
+    if (leftNums.size() < rightNums.size()) {
+      leftNums.add(rightNums.pollFirst());
     }
+  }
+  // При удалении элемента из окна, соответствующий индекс удаляется из одного из TreeSet.
+  private static void removeElement(TreeSet<Integer> leftNums, TreeSet<Integer> rightNums,
+                                    int[] nums, int i) {
+    if (rightNums.contains(i)) {
+      rightNums.remove(i);
+      if (leftNums.size() == rightNums.size() + 2) {
+        rightNums.add(leftNums.pollFirst());
+      }
+    } else {
+      leftNums.remove(i);
+      if (leftNums.size() < rightNums.size()) {
+        leftNums.add(rightNums.pollFirst());
+      }
+    }
+  }
+  private static double getMedian(TreeSet<Integer> smallNums, TreeSet<Integer> largeNums, int[] nums) {
+    if (smallNums.size() == largeNums.size()) {
+      return ((double) nums[smallNums.first()] + nums[largeNums.first()]) / 2;
+    }
+    return nums[smallNums.first()];
+  }
   // В следующем решении мы используем приоритетную очередь (кучу).
   // Здесь операция удаления в Java не оптимизирована.
   /**
    * Using Priority Queue. (Here time complexity is not optimized)
-   *
+   * <p>
    * Very similar to https://leetcode.com/problems/find-median-from-data-stream/
-   *
+   * <p>
    * Time Complexity: O((N-K)*K + N*log K).
    * Add Elements = O(N*Log K)
    * Remove Elements = O((N-K)*K) ==> PQ.remove() in JAVA is O(K)
-   *
+   * <p>
    * Space Complexity: O(K)
-   *
+   * <p>
    * N = Length of nums array. K = Input k.
    */
-    public static double[] medianSlidingWindow3(int[] nums, int k) {
-      if (nums == null || k <= 0) {
-        throw new IllegalArgumentException("Input is invalid");
-      }
-      int len = nums.length;
-      double[] result = new double[len - k + 1];
-      if (k == 1) {
-        for (int i = 0; i < len; i++) {
-          result[i] = (double) nums[i];
-        }
-        return result;
-        // return Arrays.stream(nums).asDoubleStream().toArray();
-      }
-      // MaxHeap
-      PriorityQueue<Integer> smallNums = new PriorityQueue<>(Collections.reverseOrder());
-      // Min Heap
-      PriorityQueue<Integer> largeNums = new PriorityQueue<>();
-      for (int i = 0; i < len; i++) {
-        if (i >= k) {
-          removeElem(smallNums, largeNums, nums[i - k]);
-        }
-        addElem(smallNums, largeNums, nums[i]);
-        if (i >= k - 1) {
-          result[i - (k - 1)] = getMed(smallNums, largeNums);
-        }
+  public static double[] medianSlidingWindow3(int[] nums, int k) {
+    int n = nums.length;
+    double[] result = new double[n - k + 1];
+    if (k == 1) {
+      for (int i = 0; i < n; i++) {
+        result[i] = (double) nums[i];
       }
       return result;
+      // return Arrays.stream(nums).asDoubleStream().toArray();
     }
-    private static void addElem(PriorityQueue<Integer> smallNums, PriorityQueue<Integer> largeNums, int n) {
-      smallNums.offer(n);
-      largeNums.offer(smallNums.poll());
-      if (smallNums.size() < largeNums.size()) {
-        smallNums.offer(largeNums.poll());
+    // MaxHeap
+    PriorityQueue<Integer> smallNums = new PriorityQueue<>(Collections.reverseOrder());
+    // Min Heap
+    PriorityQueue<Integer> largeNums = new PriorityQueue<>();
+    for (int i = 0; i < n; i++) {
+      if (i >= k) {
+        remove(smallNums, largeNums, nums[i - k]);
+      }
+      addElem(smallNums, largeNums, nums[i]);
+      if (i >= k - 1) {
+        result[i - (k - 1)] = median(smallNums, largeNums);
       }
     }
-    private static void removeElem(PriorityQueue<Integer> smallNums, PriorityQueue<Integer> largeNums, int n) {
-      if (n >= largeNums.peek()) {
-        largeNums.remove(n);
-        if (smallNums.size() == largeNums.size() + 2) {
-          largeNums.offer(smallNums.poll());
-        }
-      } else {
-        smallNums.remove(n);
-        if (smallNums.size() < largeNums.size()) {
-          smallNums.offer(largeNums.poll());
-        }
+    return result;
+  }
+  private static void addElem(PriorityQueue<Integer> left, PriorityQueue<Integer> right, int n) {
+    left.offer(n);
+    right.offer(left.poll());
+    if (left.size() < right.size()) {
+      left.offer(right.poll());
+    }
+  }
+  private static void remove(PriorityQueue<Integer> left, PriorityQueue<Integer> right, int n) {
+    if (n >= right.peek()) {
+      right.remove(n);
+      if (left.size() == right.size() + 2) {
+        right.offer(left.poll());
+      }
+    } else {
+      left.remove(n);
+      if (left.size() < right.size()) {
+        left.offer(right.poll());
       }
     }
-    private static double getMed(PriorityQueue<Integer> smallNums, PriorityQueue<Integer> largeNums) {
-      if (smallNums.size() == largeNums.size()) {
-        return ((double) smallNums.peek() + largeNums.peek()) / 2.0;
-      }
-      return smallNums.peek();
+  }
+  private static double median(PriorityQueue<Integer> smallNums, PriorityQueue<Integer> largeNums) {
+    if (smallNums.size() == largeNums.size()) {
+      return ((double) smallNums.peek() + largeNums.peek()) / 2.0;
     }
+    return smallNums.peek();
+  }
 }
