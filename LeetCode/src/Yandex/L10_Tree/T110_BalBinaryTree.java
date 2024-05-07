30e80334eb5e24ecbf94a3bc12636503c416a637
vovaby-web <vova_by@mail.ru>
2024-05-07 23:49:00 +0300

    Update java-119

--- LeetCode/src/Yandex/L10_Tree/T110_BalBinaryTree.java
+++ LeetCode/src/Yandex/L10_Tree/T110_BalBinaryTree.java
@@ -1,4 +1,38 @@
 // https://leetcode.com/problems/balanced-binary-tree/description/
 package Yandex.L10_Tree;
+import java.util.*;
 public class T110_BalBinaryTree {
+  public static class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+      TreeNode(int val) {
+      this.val = val;
+    }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+      this.val = val;
+      this.left = left;
+      this.right = right;
+    }
+  }
+  public static void main(String[] args) {
+    System.out.println(isBalanced(
+      new TreeNode(1, new TreeNode(2),
+        new TreeNode(2, new TreeNode(3), new TreeNode(4)))));
+  }
+  public static boolean isBalanced(TreeNode root) {
+    return height(root) >= 0;
+  }
+  private static int height(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+    int left = height(root.left);
+    int right = height(root.right);
+    if (left == -1 || right == -1 || Math.abs(left - right) > 1) {
+      return -1;
+    }
+    System.out.println(root.val+" = "+(1 + Math.max(left, right)));
+    return 1 + Math.max(left, right);
+  }
 }
