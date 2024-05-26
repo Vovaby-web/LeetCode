a657ea2dba247b99efc236e9f173df78374fa037
vovaby-web <vova_by@mail.ru>
2024-05-27 00:02:08 +0300

    Update java-130

--- src/Test.java
+++ src/Test.java
@@ -1,62 +1,19 @@
 import java.util.*;
 public class Test {
-  static class Frends {
-    String name;
-    int label;
-    boolean spic;
-    List<Frends> frends;
-    int fruit;
-    public Frends(String name) {
-      this.name = name;
-      spic = false;
-      fruit = 0;
-      frends = new ArrayList<>();
-      label=0;
-    }
-  }
   public static void main(String[] args) {
-    //                  0       1         2      3       4        5       6        7
-    String[] name = {"Вова", "Анудж", "Пегги", "Боб", "Алиса", "Клэр", "Том", "Джонни"};
-    List<Frends> list = new ArrayList<>();
-    for (int i = 0; i < name.length; i++) {
-      list.add(new Frends(name[i]));
-    }
-    list.get(0).frends.add(list.get(3));
-    list.get(0).frends.add(list.get(5));
-    list.get(0).frends.add(list.get(4));
-    list.get(1).frends.add(list.get(3));
-    list.get(2).frends.add(list.get(3));
-    list.get(2).frends.add(list.get(4));
-    list.get(3).frends.add(list.get(0));
-    list.get(3).frends.add(list.get(2));
-    list.get(3).frends.add(list.get(1));
-    list.get(4).frends.add(list.get(0));
-    list.get(4).frends.add(list.get(2));
-    list.get(5).frends.add(list.get(0));
-    list.get(5).frends.add(list.get(6));
-    list.get(5).frends.add(list.get(7));
-    list.get(6).frends.add(list.get(5));
-    list.get(7).frends.add(list.get(5));
-    list.get(3).fruit=1;
-    Queue<Frends> queue=new ArrayDeque<>();
-    int count=0;
-    list.get(0).spic=true;
-    queue.add(list.get(0));
-    while(!queue.isEmpty()){
-     Frends frends=queue.poll();
-     frends.spic=true;
-     if (frends.fruit==0){
-      for (int i=0;i<frends.frends.size();i++) {
-        if(!frends.frends.get(i).spic) {
-          frends.frends.get(i).label=frends.label+1;
-          queue.add(frends.frends.get(i));
-        }
+    String s = "tmmzuxt";
+    Map<Character, Integer> map = new HashMap<>();
+    int n = s.length();
+    int ans = 0;
+    for (int r = 0, l = 0; r < n; r++) {
+      char c = s.charAt(r);
+      if (map.containsKey(c)) {
+        if (map.get(c)>=l)
+         l = map.get(c) + 1;
       }
-     } else {
-       count=frends.label;
-       break;
-     }
+      ans = Math.max(ans, r - l + 1);
+      map.put(c, r);
     }
-    System.out.println(count);
+    System.out.println(ans);
   }
 }
---
+++ src/TopInterview150/C3_SlidingWindow/T30_SubstringwithConcatenationofAllWords.java
@@ -0,0 +1,6 @@
+// https://leetcode.com/problems/substring-with-concatenation-of-all-words/description/?envType=study-plan-v2&envId=top-interview-150
+package TopInterview150.C3_SlidingWindow;
+public class T30_SubstringwithConcatenationofAllWords {
+
+
+}
