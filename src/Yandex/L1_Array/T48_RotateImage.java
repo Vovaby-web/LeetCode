72daeaf196b08608747d7e99881647ea4ae3fb01
VovaBy-web <vova_by@mail.ru>
2025-06-12 17:01:14 +0300

    Update leetcode_3

--- src/Yandex/L1_Array/T48_RotateImage.java
+++ src/Yandex/L1_Array/T48_RotateImage.java
@@ -1,6 +1,33 @@
-package Tasks.LeetCode.Yandex.L1_Array;
+package Yandex.L1_Array;
+import java.util.Arrays;
 public class T48_RotateImage{
     public static void main(String[] args) {
-
+        int[][] a = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        System.out.println(Arrays.deepToString(rotate(a)));
+    }
+    private static int[][] rotate(int[][] a) {
+        int n = a.length;
+        int m=n-1;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n-i-1; j++) {
+                int temp = a[i][j];
+                a[i][j] = a[m-j][i];
+                a[m-j][i] = a[m-i][m-j];
+                a[m-i][m-j] = a[j][m-i];
+                a[j][m-i] = temp;
+            }
+        }
+        return a;
+    }
+    private static int[][] rotate1(int[][] a) {
+        int n = a.length;
+        int[][] b = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                b[i][(n - 1) - j] = a[j][i];
+            }
+        }
+        return b;
     }
 }
+
--- src/Yandex/L1_Array/T59_SpiralMatrixII.java
+++ src/Yandex/L1_Array/T59_SpiralMatrixII.java
@@ -1,1 +1,41 @@
-package Tasks.LeetCode.Yandex.L1_Array; public class T59_SpiralMatrixII{ }
+package Yandex.L1_Array;
+import java.util.Arrays;
+public class T59_SpiralMatrixII {
+  public static void main(String[] args) {
+    int[][] a = generateMatrix(3);
+    // Вывод на экран двумерного массива
+    Arrays.stream(a)
+       .forEach(row -> System.out.println(Arrays.toString(row)));
+  }
+  public static int[][] generateMatrix(int n) {
+    int[][] a = new int[n][n];
+    int c = 1;
+    int left = 0;
+    int right = n - 1;
+    int up = 0;
+    int down = n - 1;
+    while (up <= down || left <= right) {
+      if (up <= down) {
+        for (int j = left; j <= right; j++)
+          a[up][j] = c++;
+        up++;
+      }
+      if (left <= right) {
+        for (int j = up; j <= down; j++)
+          a[j][right] = c++;
+        right--;
+      }
+      if (up <= down) {
+        for (int j = right; j >= left; j--)
+          a[down][j] = c++;
+        down--;
+      }
+      if (left <= right) {
+        for (int j = down; j >= up; j--)
+          a[j][left] = c++;
+        left++;
+      }
+    }
+    return a;
+  }
+}
